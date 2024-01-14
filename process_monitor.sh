329c8b1596eb77a64c30e114c714bd5c7a7585ab
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-14 10:18:41 +0800

    Update process_monitor.sh

--- process_monitor.sh
+++ process_monitor.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# 从文件中获取节点名称
+node_name=$(cat /root/node_name.txt)
+
 # 定义进程名称
-PROCESS_NAME="bevm-v0.1.1-ubuntu20.04"
+process_name="bevm-v0.1.1-ubuntu20.04"
 
 # 监控进程并自动重启
 while true; do
-  if pgrep -x "$PROCESS_NAME" > /dev/null
+  if pgrep -x "$process_name" > /dev/null
   then
-    echo "$PROCESS_NAME is running"
+    echo "$process_name is running"
   else
-    echo "$PROCESS_NAME is not running, restarting..."
-    nohup /root/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="woniu4" --pruning=archive &
+    echo "$process_name is not running, restarting..."
+    nohup /root/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="$node_name" --pruning=archive &
   fi
   sleep 60  # 每隔60秒检查一次进程状态
 done
