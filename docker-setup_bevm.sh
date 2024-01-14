603db1d3ecea4c5a341a1d9d3a0863b49efa7a80
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-14 22:41:55 +0800

    Update docker-setup_bevm.sh

--- docker-setup_bevm.sh
+++ docker-setup_bevm.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# 定义节点名称文件
-NODE_NAME_FILE=/path/to/node_name.txt
+NODE_NAME_FILE=/root/node_name.txt
 
-# 生成随机节点名称
 function generate_node_name() {
   node_name=$(head /dev/urandom | tr -dc 'a-z0-9' | head -c 10)  
   echo $node_name
 }
 
-# 运行Docker容器
 function run_docker_containers() {
 
   read -p "请输入要运行的Docker容器数量:" container_count
@@ -20,7 +17,7 @@ function run_docker_containers() {
 
     sudo docker run -d btclayer2/bevm:v0.1.1 bevm --chain=testnet --name="$node_name" --pruning=archive --telemetry-url "wss://telemetry.bevm.io/submit 0"
 
-    echo $node_name > $NODE_NAME_FILE
+    echo $node_name >> $NODE_NAME_FILE
 
   done
 
@@ -36,10 +33,9 @@ sudo apt install docker.io
 sudo docker pull btclayer2/bevm:v0.1.1
 
 # 运行节点
-run_docker_containers
+run_docker_containers 
 
-# 输出结果
-node_name=$(cat $NODE_NAME_FILE)
+node_name=$(tail -n 1 $NODE_NAME_FILE)
 echo "部署完成,节点名称:$node_name,并且保存在$NODE_NAME_FILE文件中"
 
 echo "部署完成!"
