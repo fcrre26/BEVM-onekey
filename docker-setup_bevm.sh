11db73c4f25652b1a0f000ae62c78176f3ca51eb
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-16 19:32:20 +0800

    Update docker-setup_bevm.sh

--- docker-setup_bevm.sh
+++ docker-setup_bevm.sh
@@ -9,6 +9,18 @@ sudo ufw allow 8086
 sudo ufw allow 8087
 sudo ufw status
 
+# 安装 Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# 检查docker命令是否可用
+docker_cmd=$(which docker)
+
+if ! command -v $docker_cmd &> /dev/null; then
+  echo "请先安装docker"
+  exit 1
+fi
+
 # 获取节点数量
 read -p "请输入节点数量: " count
 
@@ -28,24 +40,6 @@ for i in $(seq 1 $count); do
   sudo docker run -d -v /var/lib/node_bevm_test_storage:/root/.local/share/bevm --name $name btclayer2/bevm:v0.1.1 bevm --chain=testnet --name="$name" --pruning=archive --telemetry-url "wss://telemetry.bevm.io/submit 0"
   echo "容器 $name 启动完成"
 done
-
-# 安装 Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-# 检查docker命令是否可用
-docker_cmd=$(which docker)
-
-if ! command -v $docker_cmd &> /dev/null; then
-  echo "请先安装docker"
-  exit 1
-fi
-
-# 赋予脚本执行权限
-chmod +x $0
-
-# 运行容器
-runContainers
 
 # 输出部署完成的消息以及节点列表
 echo "部署完成,节点列表:"
