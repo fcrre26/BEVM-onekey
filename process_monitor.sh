d840fb9ac7deb725f5a3280260f51180381d372f
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-16 08:56:06 +0800

    Update process_monitor.sh

--- process_monitor.sh
+++ process_monitor.sh
@@ -6,14 +6,17 @@ node_name=$(cat /root/node_name.txt)
 # 定义进程名称
 process_name="bevm-v0.1.1-ubuntu20.04"
 
+# 定义日志文件路径
+log_file="/var/log/process_monitor.log"
+
 # 监控进程并自动重启
 while true; do
   if pgrep -x "$process_name" > /dev/null
   then
-    echo "$process_name is running"
+    echo "$(date): $process_name is running" >> $log_file
   else
-    echo "$process_name is not running, restarting..."
-    nohup /root/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="$node_name" --pruning=archive --telemetry-url "wss://telemetry.bevm.io/submit 0" &
+    echo "$(date): $process_name is not running, restarting..." >> $log_file
+    nohup /root/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="$node_name" --pruning=archive --telemetry-url "wss://telemetry.bevm.io/submit 0" >> $log_file 2>&1 &
   fi
   sleep 60  # 每隔60秒检查一次进程状态
 done
