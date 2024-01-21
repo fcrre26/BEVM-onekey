8d8f5d9037e53323fb976872c6253d42f5720ebd
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-21 11:13:18 +0800

    Update cancel_all_limits.sh

--- cancel_all_limits.sh
+++ cancel_all_limits.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 # 询问用户是否确定取消所有容器的内存和交换空间限制
-read -p "你确定要取消所有容器的内存以及交换空间限制吗？(Y继续): " answer
-if [ "$answer" != "Y" ]; then
+read -p "你确定要取消所有容器的内存以及交换空间限制吗？(Y继续): " confirm
+if [ "$confirm" != "Y" ]; then
   echo "取消操作已取消。"
   exit 0
 fi
 
-# ... 检索所有的容器
+# 检索所有的容器
 echo "正在检索所有的容器..."
 if ! docker ps -a; then
   echo "无法检索容器，请检查Docker是否正确安装。"
   exit 1
 fi
 
-# ... 取消所有容器的内存和交换空间限制
+# 取消所有容器的内存和交换空间限制
 echo "正在取消所有容器的内存和交换空间限制..."
 container_ids=$(docker ps -aq)
 for container_id in $container_ids; do
-  if ! docker update --memory="" ... --memory-swap="" $container_id; then
+  if ! docker update --memory="" --memory-swap="" $container_id; then
     echo "无法取消容器的内存和交换空间限制，请检查Docker是否正确安装。"
     exit 1
   fi
 done
-echo ... "所有容器的内存和交换空间限制已成功取消。"
+
+echo "所有容器的内存和交换空间限制已成功取消。"
