49afb9760f5e91e474c176ddb0f758b892118b6c
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-21 11:25:19 +0800

    Update cancel_all_limits.sh

--- cancel_all_limits.sh
+++ cancel_all_limits.sh
@@ -18,7 +18,7 @@ fi
 echo "正在取消所有容器的内存和交换空间限制..."
 container_ids=$(docker ps -aq)
 for container_id in $container_ids; do
-  if ! docker update --memory="" --memory-swap="" $container_id; then
+  if ! docker update --memory=0 --memory-swap=0 $container_id; then
     echo "无法取消容器的内存和交换空间限制，请检查Docker是否正确安装。"
     exit 1
   fi
