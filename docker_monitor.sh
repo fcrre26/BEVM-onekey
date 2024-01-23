b0e7e978f84781bf2fe547973eb5b3859171ecb9
fcrre26 <123922394+fcrre26@users.noreply.github.com>
2024-01-24 00:09:26 +0800

    Update docker_monitor.sh

--- docker_monitor.sh
+++ docker_monitor.sh
@@ -3,9 +3,6 @@
 # 定义日志文件路径
 LOG_FILE="/root/docker_status.log"
 
-# 打印提示信息
-echo "docker守护已经成功开启，可以随时查看日志！"
-
 # 检查容器状态并记录日志函数
 check_container_status() {
   container_status=$(docker ps -q | xargs docker inspect -f '{{.State.Running}}' 2>/dev/null)
@@ -50,6 +47,9 @@ EOF
 # 启用并启动服务
 systemctl enable docker_monitor.service
 systemctl start docker_monitor.service
+
+# 打印提示信息
+echo "docker守护已经成功开启，可以随时查看日志！"
 
 # 每分钟自动检查容器状态
 # 这里使用while循环，每隔60秒执行一次检查
